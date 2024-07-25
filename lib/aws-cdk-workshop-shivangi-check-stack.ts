9c2d74ed0b6b6333b0f62f1effffc2546fa793e6
ShivangiAsthanaAccolite <shivangi.asthana@accolitedigital.com>
2024-07-25 20:27:39 +0530

    polish pipeline

--- lib/aws-cdk-workshop-shivangi-check-stack.ts
+++ lib/aws-cdk-workshop-shivangi-check-stack.ts
@@ -9,6 +9,8 @@ import { HitCounter } from "./hitcounter";
 import { TableViewer } from "cdk-dynamo-table-viewer";
 
 export class AwsCdkWorkshopShivangiCheckStack extends Stack {
+  public readonly hcViewerUrl: cdk.CfnOutput;
+  public readonly hcEndpoint: cdk.CfnOutput;
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
@@ -24,14 +26,22 @@ export class AwsCdkWorkshopShivangiCheckStack extends Stack {
     });
 
     // defines an API Gateway REST API resource backed by our "hello" function.
-    new apigw.LambdaRestApi(this, "Endpoint", {
+    const gateway = new apigw.LambdaRestApi(this, "Endpoint", {
       handler: helloWithCounter.handler,
     });
 
-    new TableViewer(this, "ViewHitCounter", {
+    const tv = new TableViewer(this, "ViewHitCounter", {
       title: "Hello Hits",
       table: helloWithCounter.table,
       sortBy: "-hits",
+    });
+
+    this.hcEndpoint = new cdk.CfnOutput(this, "GatewayUrl", {
+      value: gateway.url,
+    });
+
+    this.hcViewerUrl = new cdk.CfnOutput(this, "TableViewerUrl", {
+      value: tv.endpoint,
     });
   }
 }
