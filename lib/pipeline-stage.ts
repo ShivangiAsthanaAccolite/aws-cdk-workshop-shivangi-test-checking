c0735a67d7cb3a764565873fce0b1cd3572b3cf0
ShivangiAsthanaAccolite <shivangi.asthana@accolitedigital.com>
2024-07-25 20:36:12 +0530

    validation test

--- lib/pipeline-stage.ts
+++ lib/pipeline-stage.ts
@@ -1,12 +1,21 @@
-import { Stage, StageProps } from "aws-cdk-lib";
+import { CfnOutput, Stage, StageProps } from "aws-cdk-lib";
 
 import { AwsCdkWorkshopShivangiCheckStack } from "./aws-cdk-workshop-shivangi-check-stack";
 import { Construct } from "constructs";
 
 export class WorkshopPipelineStage extends Stage {
+  public readonly hcViewerUrl: CfnOutput;
+  public readonly hcEndpoint: CfnOutput;
+
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
 
-    new AwsCdkWorkshopShivangiCheckStack(this, "WebServiceSACheck");
+    const service = new AwsCdkWorkshopShivangiCheckStack(
+      this,
+      "WebServiceSACheck"
+    );
+
+    this.hcEndpoint = service.hcEndpoint;
+    this.hcViewerUrl = service.hcViewerUrl;
   }
 }
