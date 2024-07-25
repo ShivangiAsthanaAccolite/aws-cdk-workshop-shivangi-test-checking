d78a29958c6fabb05ac928b566543bfbce7162ba
ShivangiAsthanaAccolite <shivangi.asthana@accolitedigital.com>
2024-07-25 19:21:35 +0530

    pipeline

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -18,6 +18,17 @@ export class WorkshopPipelineStack extends cdk.Stack {
       repositoryName: "WorkshopRepoSACheck",
     });
 
+    // The basic pipeline declaration. This sets the initial structure
+    // of our pipeline
+    const pipeline = new CodePipeline(this, "Pipeline", {
+      pipelineName: "WorkshopPipelineSACheck",
+      synth: new CodeBuildStep("SynthStep", {
+        input: CodePipelineSource.codeCommit(repo, "main"),
+        installCommands: ["npm install -g aws-cdk"],
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
+    });
+
     // Pipeline code goes here
   }
 }
