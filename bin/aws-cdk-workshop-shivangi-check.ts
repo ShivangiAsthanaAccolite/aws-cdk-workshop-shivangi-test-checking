5b7444440ca78a54aa1ba4b2130d51c051d3286d
ShivangiAsthanaAccolite <shivangi.asthana@accolitedigital.com>
2024-08-02 16:33:42 +0530

    adding back stack and commenting pipeline

--- bin/aws-cdk-workshop-shivangi-check.ts
+++ bin/aws-cdk-workshop-shivangi-check.ts
@@ -2,14 +2,22 @@
 
 import * as cdk from "aws-cdk-lib";
 
-import { WorkshopPipelineStack } from "../lib/pipeline-stack";
+// import { WorkshopPipelineStack } from "../lib/pipeline-stack";
+import { AwsCdkWorkshopShivangiCheckStack } from "../lib/aws-cdk-workshop-shivangi-check-stack";
 
 const app = new cdk.App();
 // new WorkshopPipelineStack(app, "CdkWorkshopPipelineStackSACheck");
 
-new WorkshopPipelineStack(app, "CdkWorkshopPipelineStackSACheck", {
+new AwsCdkWorkshopShivangiCheckStack(app, "CdkWorkshopPipelineStackSACheck", {
   env: {
     account: process.env.account,
     region: process.env.region,
   },
 });
+
+// new WorkshopPipelineStack(app, "CdkWorkshopPipelineStackSACheck", {
+//   env: {
+//     account: process.env.account,
+//     region: process.env.region,
+//   },
+// });
