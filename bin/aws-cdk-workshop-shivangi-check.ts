860fcfe71a6ca3a1c06aece85749a926d04dac41
ShivangiAsthanaAccolite <shivangi.asthana@accolitedigital.com>
2024-08-02 16:27:46 +0530

    adding account and region

--- bin/aws-cdk-workshop-shivangi-check.ts
+++ bin/aws-cdk-workshop-shivangi-check.ts
@@ -5,4 +5,11 @@ import * as cdk from "aws-cdk-lib";
 import { WorkshopPipelineStack } from "../lib/pipeline-stack";
 
 const app = new cdk.App();
-new WorkshopPipelineStack(app, "CdkWorkshopPipelineStackSACheck");
+// new WorkshopPipelineStack(app, "CdkWorkshopPipelineStackSACheck");
+
+new WorkshopPipelineStack(app, "CdkWorkshopPipelineStackSACheck", {
+  env: {
+    account: "590376488337",
+    region: "us-east-1",
+  },
+});
