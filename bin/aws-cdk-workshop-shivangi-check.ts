dbc5594330e81cbff4f66fd16ba08f2c3de9ed92
ShivangiAsthanaAccolite <shivangi.asthana@accolitedigital.com>
2024-08-02 16:45:19 +0530

    changing back to pipeline

--- bin/aws-cdk-workshop-shivangi-check.ts
+++ bin/aws-cdk-workshop-shivangi-check.ts
@@ -2,22 +2,23 @@
 
 import * as cdk from "aws-cdk-lib";
 
-// import { WorkshopPipelineStack } from "../lib/pipeline-stack";
-import { AwsCdkWorkshopShivangiCheckStack } from "../lib/aws-cdk-workshop-shivangi-check-stack";
+import { WorkshopPipelineStack } from "../lib/pipeline-stack";
+
+// import { AwsCdkWorkshopShivangiCheckStack } from "../lib/aws-cdk-workshop-shivangi-check-stack";
 
 const app = new cdk.App();
 // new WorkshopPipelineStack(app, "CdkWorkshopPipelineStackSACheck");
 
-new AwsCdkWorkshopShivangiCheckStack(app, "AwsCdkWorkshopShivangiCheckStack", {
-  env: {
-    account: process.env.account,
-    region: process.env.region,
-  },
-});
-
-// new WorkshopPipelineStack(app, "CdkWorkshopPipelineStackSACheck", {
+// new AwsCdkWorkshopShivangiCheckStack(app, "AwsCdkWorkshopShivangiCheckStack", {
 //   env: {
 //     account: process.env.account,
 //     region: process.env.region,
 //   },
 // });
+
+new WorkshopPipelineStack(app, "CdkWorkshopPipelineStackSACheck", {
+  env: {
+    account: process.env.account,
+    region: process.env.region,
+  },
+});
